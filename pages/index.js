2e3ae8099aed01bcc4310372211f318310cd24df
Ahmed React-dev <mwacode@gmail.com>
2022-04-17 14:16:30 +0000

    New project

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>AhmedREV - Store</title>
+        <meta name="description" content="AhmedREV - E-STORE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     Hey this is Ahmedstore
+
+ 
+     </div>
+  )
+}
